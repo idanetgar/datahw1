027d9e25cf2c6a149061248b671ec62d288c033b
idanetgar <idanetgar@campus.technion.ac.il>
2021-04-07 18:09:12 +0300

    hi bitchhhhh

---
+++ statistics.py
@@ -0,0 +1,25 @@
+def sum(values):
+    sum = 0
+    for i in values:
+        sum += i
+
+    return sum
+
+def mean(values):
+    sum = 0
+    for i in values:
+        sum += i
+
+    mean = sum/len(values)
+    return mean
+
+def median(values):
+    med = 0
+    values.sort()
+    n = len(values)
+    if n%2:
+        med = values[(n+1)/2]
+    else:
+        med = (values[n/2] + values[(n+1)/2])/2
+
+    return med
